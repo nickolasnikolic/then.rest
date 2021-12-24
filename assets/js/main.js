6e207dbe38431cf14e09d582a8421b52098d5dcb
Nickolas Nikolic <nick@rubbery.fun>
2021-12-24 12:41:29 -0600

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -24,11 +24,40 @@ for(let staffLine = 1; staffLine < 6; staffLine++){
 
 // create notes
 class MusicalNote{
-
+    constructor(pitch, startTime, duration){
+        this.pitch = pitch;
+        this.startTime = startTime;
+        this.duration = duration;
+    }
 }
 
 // turn the interface
+
 // upon click, set the note location and time of click, and length of time of click
+let queNotes = [];
+window.addEventListener('mousedown', (e_down) => {
+    //get time of click
+    let startTime = new Date().getTime();
+
+    //get location of click and seive it into note slots
+    // firstly, we already have center of circles
+    // secondly, devide by the staff lines the larger distance click x or y from center x or y
+    //wait for mouseup
+    window.addEventListener('mouseup', (e_up) => {
+        const distanceFromCenterX = centerX - e_down.clientX;
+        const distanceFromCenterY = centerY - e_down.clientY;
+
+        const maxDistance = Math.max(distanceFromCenterX,distanceFromCenterY);
+
+        //pitch is one of 12 tones within the scale, this is arbitrary
+        const pitch = maxDistance / 12;
+
+        let duration = Math.abs(startTime - (new Date().getTime()));
+        queNotes.push(new MusicalNote(pitch, startTime, duration)); //todo pitch is placeholder
+        console.log(queNotes);
+    });
+});
+
 // upon return to level position, play the note for the duration remebered
 
 };
