37f28756b6e83e3743e900bdb8ff7e974b06bf21
Nickolas Nikolic <nick@rubbery.fun>
2021-12-24 11:05:12 -0600

    boilerplate

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+window.onload = () => {
+
+// get reference to the stage
+// create circular interface
+// create notes
+// turn the interface
+// upon click, set the note location and time of click, and length of time of click
+// upon return to level position, play the note for the duration remebered
+
+};
